50e818e2c6473bc8fb0903d3f7d5eee84417cdfc
Dhanajit Kapali <dhanajit.kapali@unthinkable.co>
2022-05-20 16:39:43 +0530

    added the TabBarController

--- TikTokClone/Controllers/Core/HomeViewController.swift
+++ TikTokClone/Controllers/Core/HomeViewController.swift
@@ -12,8 +12,12 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        
+        
+        
     }
+    
 
 
 }
