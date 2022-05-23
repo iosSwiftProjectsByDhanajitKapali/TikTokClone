50877a1f30b7cf601561e3c8bb8fc846463e4c8f
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-05-23 22:24:22 +0530

    added the liked heart animation

--- TikTokClone/Controllers/Other/PostViewController.swift
+++ TikTokClone/Controllers/Other/PostViewController.swift
@@ -9,13 +9,39 @@ import UIKit
 
 class PostViewController: UIViewController {
 
-    let model : PostModel
+    var model : PostModel
     
+    // MARK: - UI Components
+    private let likeButton : UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "heart.fill"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.tintColor = .white
+        return button
+    }()
+    
+    private let commentButton : UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "text.bubble.fill"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.tintColor = .white
+        return button
+    }()
+    
+    private let shareButton : UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.tintColor = .white
+        return button
+    }()
+    
+    
+    //Initializers
     init(model : PostModel) {
         self.model = model
         super.init(nibName: nil, bundle: nil)
     }
-    
     required init?(coder : NSCoder) {
         fatalError()
     }
@@ -30,7 +56,102 @@ extension PostViewController {
         let colors : [UIColor] = [
             .red, .green, .orange, .blue, .white, .systemPink
         ]
-        
         view.backgroundColor = colors.randomElement()
+        
+        setUpButtons()
+        setUpDoubleTapToLike()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let size : CGFloat = 40
+        let yStart : CGFloat = view.height - (size * 4) - 30 - view.safeAreaInsets.bottom - (tabBarController?.tabBar.height ?? 0)
+        for(index, button) in [likeButton, commentButton, shareButton].enumerated(){
+            button.frame = CGRect(
+                x: view.width-size-20,
+                y: yStart + (CGFloat(index) * size) + (CGFloat(index) * 10),
+                width: size,
+                height: size
+            )
+        }
+    }
+}
+
+
+// MARK: - Private Methods
+private extension PostViewController{
+    
+    func setUpButtons(){
+        view.addSubview(likeButton)
+        view.addSubview(commentButton)
+        view.addSubview(shareButton)
+        
+        likeButton.addTarget(self, action: #selector(didTapLikeButton), for: .touchUpInside)
+        commentButton.addTarget(self, action: #selector(didTapCommentButton), for: .touchUpInside)
+        shareButton.addTarget(self, action: #selector(didTapShareButton), for: .touchUpInside)
+        
+        
+    }
+    
+    @objc func didTapLikeButton(){
+        model.isLikedByCurrentUser = !model.isLikedByCurrentUser
+        likeButton.tintColor = model.isLikedByCurrentUser ? .systemRed : .white
+    }
+    
+    @objc func didTapCommentButton(){
+        
+    }
+    
+    @objc func didTapShareButton(){
+        guard let url = URL(string: "https://www.tiktok.com") else {
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [url], applicationActivities: [])
+        present(vc, animated: true)
+    }
+    
+    func setUpDoubleTapToLike(){
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didDoubleTap(_:)))
+        tap.numberOfTapsRequired = 2
+        view.addGestureRecognizer(tap)
+        view.isUserInteractionEnabled = true
+    }
+    
+    @objc func didDoubleTap(_ gesture : UITapGestureRecognizer){
+        if !model.isLikedByCurrentUser {
+            model.isLikedByCurrentUser = true
+        }
+        
+        let touchPoint = gesture.location(in: view)
+        
+        //Add Liked Heart Animation
+        let imageView = UIImageView(image: UIImage(systemName: "heart.fill"))
+        imageView.tintColor = .systemRed
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        imageView.center = touchPoint
+        imageView.contentMode = .scaleAspectFit
+        imageView.alpha = 0
+        view.addSubview(imageView)
+        
+        
+        UIView.animate(withDuration: 0.2) {
+            imageView.alpha = 1
+        } completion: { done in
+            if done{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    UIView.animate(withDuration: 0.3) {
+                        imageView.alpha = 0
+                    } completion: { done in
+                        if done{
+                            imageView.removeFromSuperview()
+                        }
+                    }
+                }
+                
+            }
+        }
+
     }
 }
