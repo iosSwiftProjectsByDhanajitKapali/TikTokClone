6a7b53880ebbd562317139f0ff722229aa26b24e
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-06-07 20:55:24 +0530

    added signOut Button

--- TikTokClone/Controllers/Other/SettingsViewController.swift
+++ TikTokClone/Controllers/Other/SettingsViewController.swift
@@ -9,10 +9,101 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    // MARK: - UI Components
+    private let tableView : UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+
+}
+
+
+// MARK: - LifeCycle Methods
+extension SettingsViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .secondarySystemBackground
+        title = "Settings"
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        createFooter()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+}
 
+            
+// MARK: - Private Methods
+private extension SettingsViewController {
+    
+    func createFooter() {
+        let footer = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: 100))
+        let button = UIButton(frame: CGRect(x: (view.width-200)/2, y: 25, width: 200, height: 50))
+        button.setTitle("Sign Out", for: .normal)
+        button.setTitleColor(.systemRed, for: .normal)
+        button.addTarget(self, action: #selector(didTapSignOut), for: .touchUpInside)
+        footer.addSubview(button)
+        tableView.tableFooterView = footer
+    }
+    
+    @objc func didTapSignOut() {
+        let actionSheet = UIAlertController(
+            title: "SignOut",
+            message: "Would you like to SignOut?",
+            preferredStyle: .actionSheet
+        )
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        actionSheet.addAction(UIAlertAction(title: "SignOut", style: .destructive, handler: {[weak self] _ in
+            DispatchQueue.main.async {
+                AuthManager.shared.signOut { sucess in
+                    if sucess {
+                        UserDefaults.standard.setValue(nil, forKey: "username")
+                        UserDefaults.standard.setValue(nil, forKey: "profile_picture_url")
+                        
+                        let vc = SignInViewController()
+                        let navVC = UINavigationController(rootViewController: vc)
+                        navVC.modalPresentationStyle = .fullScreen
+                        self?.present(navVC, animated: true)
+                        
+                        self?.navigationController?.popToRootViewController(animated: true)
+                        self?.tabBarController?.selectedIndex = 0
+                    }else {
+                        //Failed to SignOut
+                        let alert = UIAlertController(
+                            title: "Woops",
+                            message: "Something went wrong while signing out. Please try again later!",
+                            preferredStyle: .alert
+                        )
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                        self?.present(alert, animated: true)
+                    }
+                }
+            }
+            
+        }))
+        present(actionSheet, animated: true)
+    }
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource Methods
+extension SettingsViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
+    
 }
